b404e346f637d1455b54be96361347c8df02abd5
Andohenri <andohenrirazafinatoandro@gmail.com>
2024-07-31 22:05:16 +0300

    Page de demmarage en place

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,19 @@
 import { Image } from 'react-native'
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { Link } from "expo-router";
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from '@/components/CustomButton';
 
 export default function Index() {
   return (
     <SafeAreaView style={{ height: '100%', backgroundColor: '#161622' }}>
-      <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Image source={{uri: ""}} resizeMode='contain' style={{}} />
-        <ThemedText type="subtitle" >Edit app/index.tsx to edit this screen.</ThemedText>
-        <ThemedText type="link"><Link href={'/home'}>Go to Home</Link></ThemedText>
+      <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 16 }}>
+        <Image source={require('../assets/images/logo.png')} resizeMode='contain' style={{ height: 300, width: 300, marginBottom: 24 }} />
+        <ThemedText type="title" style={{ marginBottom: 16, textAlign: 'center' }}>Bienvenue sur MapMaker</ThemedText>
+        <ThemedText type="subtitle" style={{ marginBottom: 16, textAlign: 'center' }}>Découvrer l'application ultime pour visualiser les places interéssants dans le monde.</ThemedText>
+        <CustomButton title={"Allez découvrir"} handlePress={() => router.push('/home')} />
       </ThemedView>
       <StatusBar style="light" />
     </SafeAreaView>
---
+++ components/CustomButton.tsx
@@ -0,0 +1,16 @@
+import { Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { ThemedText } from './ThemedText'
+
+const CustomButton = ({ title, handlePress }: any) => {
+   return (
+      <TouchableOpacity
+         onPress={handlePress}
+         style={{ backgroundColor: '#FFA001', borderRadius: 24, minHeight: 60, alignItems: 'center', justifyContent: 'center', width: '100%' }}
+      >
+         <ThemedText style={{ color: '#161622', fontWeight: '700', fontSize: 24 }}>{title}</ThemedText>
+      </TouchableOpacity>
+   )
+}
+
+export default CustomButton
